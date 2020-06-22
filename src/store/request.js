8133df03e35b9522b66f2ba24e8507c221162730
gitzky <927926261@qq.com>
2020-06-22 18:48:16 +0800

    update

--- src/store/request.js
+++ src/store/request.js
@@ -1,50 +1,50 @@
-import axios from "axios";
+import axios from 'axios'
 // 创建axios实例
-console.log(process.env);
+console.log(process.env)
 const request = axios.create({
-  baseURL: "/", // api的base_url
+  baseURL: '/', // api的base_url
   timeout: 5000, // 请求超时时间
   withCredentials: true, //跨域
   headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
   },
   transformRequest: [
-    function(data) {
-      let req = JSON.stringify(data);
-      return req;
-    }
-  ]
-});
+    function (data) {
+      let req = JSON.stringify(data)
+      return req
+    },
+  ],
+})
 
 request.interceptors.response.use(
-  response => {
-    const status = response.status;
+  (response) => {
+    const status = response.status
 
     if (status === 401) {
-      router.push("/401");
-      return false;
+      router.push('/401')
+      return false
     }
     if (status === 403) {
-      router.push("/403");
-      return false;
+      router.push('/403')
+      return false
     }
     if (status <= 504 && status >= 500) {
-      router.push("/500");
-      return false;
+      router.push('/500')
+      return false
     }
     if (status >= 404 && status < 422) {
-      router.push("/404");
-      return false;
+      router.push('/404')
+      return false
     }
 
-    const res = response.data;
+    const res = response.data
 
-    return res;
+    return res
   },
-  error => {
-    return Promise.reject(error);
-  }
-);
+  (error) => {
+    return Promise.reject(error)
+  },
+)
 
-export default request;
+export default request
