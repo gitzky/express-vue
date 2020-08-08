5fad529f3460808519ea26abbaaa01fc4dcf8700
gitzky <927926261@qq.com>
2020-08-08 17:30:12 +0800

    update port 更改为80端口

--- vue.config.js
+++ vue.config.js
@@ -1,29 +1,29 @@
-const path = require("path");
+const path = require('path')
 
 module.exports = {
   // 基本路径
-  publicPath: "./",
+  publicPath: './',
   // 输出文件目录
-  outputDir: "dist",
-  assetsDir: "assets", // 静态资源目录 (js, css, img, fonts)
+  outputDir: 'dist',
+  assetsDir: 'assets', // 静态资源目录 (js, css, img, fonts)
 
-  lintOnSave: process.env.NODE_ENV === "development",
+  lintOnSave: process.env.NODE_ENV === 'development',
   devServer: {
-    port: 8800,
+    port: 80,
     open: true,
     overlay: {
       warnings: false,
-      errors: true
+      errors: true,
     },
     proxy: {
-      "/api": {
+      '/api': {
         target: process.env.VUE_APP_API, //对应自己的接口
         changeOrigin: true,
         ws: true,
         pathRewrite: {
-          "^/api": "/api"
-        }
-      }
-    }
-  }
-};
+          '^/api': '/api',
+        },
+      },
+    },
+  },
+}
